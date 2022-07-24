1706763d763df44a764dd18dca6fcb6f621a20be
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-07-24 14:04:07 +0530

    Initial commit

---
+++ src/cyclicSort/FindAllNoDissappearedInArray.java
@@ -0,0 +1,36 @@
+package cyclicSort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllNoDissappearedInArray {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }
+            else
+                i++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+
+            if(nums[index] != index+1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+    public static void swap(int[] arr,int first, int correct){
+        int temp = arr[first];
+        arr[first] = arr[correct];
+        arr[correct] = temp;
+    }
+}
