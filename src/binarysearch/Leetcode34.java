a529cdedbbe8d57ded43a3e92ccc38b279c9e5e2
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-07-28 14:52:42 +0530

    Initial commit

---
+++ src/binarysearch/Leetcode34.java
@@ -0,0 +1,43 @@
+package binarysearch;
+
+import java.util.Arrays;
+
+public class Leetcode34 {
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,7,7,7,8,10};
+        int target = 7;
+        System.out.println(Arrays.toString(searchRange(arr,target)));
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int start = search(nums, target,true);
+        int end = search(nums,target,false);
+        ans[0] = start; ans[1] = end;
+        return ans;
+    }
+    static int search(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] > target){
+                end = mid-1;
+
+            }
+            else if (nums[mid] < target){
+                start = mid+1;
+            }
+            else {
+                ans = mid;
+                if(findStartIndex){
+                    end = mid-1;
+                }
+                else
+                    start = mid+1;
+            }
+        }
+        return ans;
+    }
+}
