f65bcc5b1e583f516b277f25124c7c2558ea045f
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-08-08 17:02:11 +0530

    Initial commit

---
+++ src/bitwiseOperators/FindUnique.java
@@ -0,0 +1,16 @@
+package bitwiseOperators;
+
+public class FindUnique {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6,5,4,3,2};
+        System.out.println(ans(arr));
+    }
+
+    private static int ans(int[] arr) {
+        int unique = 0;
+        for(int n : arr){
+            unique ^= n;
+        }
+        return unique;
+    }
+}
