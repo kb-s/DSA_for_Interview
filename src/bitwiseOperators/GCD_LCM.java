2dea7e242868b03e56d8f365331916d94218fb13
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-08-08 17:04:30 +0530

    Initial commit

---
+++ src/bitwiseOperators/GCD_LCM.java
@@ -0,0 +1,16 @@
+package bitwiseOperators;
+
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(lcm(7,8));
+    }
+    static int gcd(int a, int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    static int lcm(int a, int b){
+        return a*b/gcd(a,b);
+    }
+}
