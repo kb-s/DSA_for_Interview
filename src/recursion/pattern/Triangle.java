8c4b3d242289d352148558b982d9ead1bec51c0e
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-08-11 15:00:09 +0530

    Initial commit

---
+++ src/recursion/pattern/Triangle.java
@@ -0,0 +1,23 @@
+package recursion.pattern;
+
+public class Triangle {
+    public static void main(String[] args) {
+        triangle(4,0);
+    }
+    static void triangle(int r, int c){
+        if(r==0){
+            return;
+
+        }
+        if(r>c){
+            /*System.out.print("*");*/
+            triangle(r,c+1);
+            System.out.print("*");
+        }
+        else{
+            triangle(r-1,0);
+            System.out.println();
+            /*triangle(r-1,0);*/
+        }
+    }
+}
