9bf554c0e649edd07fda9b32338a5efcad17bb3e
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-08-16 16:41:37 +0530

    Initial commit

---
+++ src/recursion/string/SubSeq.java
@@ -0,0 +1,17 @@
+package recursion.string;
+
+public class SubSeq {
+    public static void main(String[] args) {
+        subSeq("","abc");
+    }
+    static void subSeq(String p, String up){
+        if(up.isEmpty()){
+            System.out.print(p+" ");
+            return;
+        }
+        char ch = up.charAt(0);
+        subSeq(p+ch,up.substring(1));
+        subSeq(p,up.substring(1));
+    }
+}
+
