7f1e476383b42466346d43fe3e74218febf14d5f
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2022-09-08 16:22:22 +0530

    Initial commit

--- src/stackandqueue/QueueMain.java
+++ src/stackandqueue/QueueMain.java
@@ -1,15 +1,21 @@
 package stackandqueue;
 
 public class QueueMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CustomQueue customQueue = new CustomQueue();
-        customQueue.insert(5);
-        customQueue.insert(3);
-        customQueue.insert(4);
-        customQueue.insert(2);
-        customQueue.insert(15);
+        CircularQueue queue = new CircularQueue(5);
+        queue.insert(3);
+        queue.insert(6);
+        queue.insert(5);
+        queue.insert(19);
+        queue.insert(1);
 
-        customQueue.display();
+        queue.display();
+
+        System.out.println(queue.remove());
+        queue.insert(133);
+        queue.display();
+
 
     }
 }
